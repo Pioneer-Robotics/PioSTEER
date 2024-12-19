7195d486a99389b796761073fd2561a4ebbef722
Michael Gross <mgstinger521@gmail.com>
2024-12-18 18:58:46 -0700

    add favicon. remove darkmode

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_file, send_from_directory
+from flask import Flask, send_file, send_from_directory
 
 app = Flask(__name__)
 
@@ -16,27 +16,27 @@ def index():
 def scripts(name):
     return send_from_directory('scripts', name)
 
-@app.route('/get_points', methods=['GET'])
-def get_points():
-    return jsonify(control_points=control_points)
+# @app.route('/get_points', methods=['GET'])
+# def get_points():
+#     return jsonify(control_points=control_points)
 
-@app.route('/clear_points', methods=['POST'])
-def clear_points():
-    control_points.clear()
-    return jsonify(success=True)
+# @app.route('/clear_points', methods=['POST'])
+# def clear_points():
+#     control_points.clear()
+#     return jsonify(success=True)
 
-@app.route('/export_points', methods=['GET'])
-def export_points():
-    # Normalize the points before exporting
-    normalized_points = []
+# @app.route('/export_points', methods=['GET'])
+# def export_points():
+#     # Normalize the points before exporting
+#     normalized_points = []
 
-    # Normalize control points to fit the 0-366 range
-    for x, y in control_points:
-        normalized_x = (x / img_width) * 366
-        normalized_y = (y / img_height) * 366
-        normalized_points.append((normalized_x, normalized_y))
+#     # Normalize control points to fit the 0-366 range
+#     for x, y in control_points:
+#         normalized_x = (x / img_width) * 366
+#         normalized_y = (y / img_height) * 366
+#         normalized_points.append((normalized_x, normalized_y))
 
-    return jsonify(control_points=normalized_points)
+#     return jsonify(control_points=normalized_points)
 
 if __name__ == '__main__':
     app.run(debug=True)
